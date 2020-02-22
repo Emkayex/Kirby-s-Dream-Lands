ab93c2ef23dbf2275ffb8efd46413ad2db507a8e
Emkayex <53287857+Emkayex@users.noreply.github.com>
2020-02-22 12:00:48 -0600

    Started work on customization of Kirby colors

---
+++ Scripts/AutoLoad/PlayerColors.gd
@@ -0,0 +1,26 @@
+extends Node
+
+func _ready():
+	pass # Replace with function body.
+
+var PlayerColors = [
+	# Player 1 Colors
+	{"normal_primary" : "", "normal_secondary" : "", "normal_tertiary" : "",
+	"ability_primary" : "", "ability_secondary" : "", "ability_tertiary" : "",
+	"ice_primary" : "", "ice_secondary" : "", "ice_tertiary" : ""},
+
+	# Player 2 Colors
+	{"normal_primary" : "", "normal_secondary" : "", "normal_tertiary" : "",
+	"ability_primary" : "", "ability_secondary" : "", "ability_tertiary" : "",
+	"ice_primary" : "", "ice_secondary" : "", "ice_tertiary" : ""},
+
+	# Player 3 Colors
+	{"normal_primary" : "", "normal_secondary" : "", "normal_tertiary" : "",
+	"ability_primary" : "", "ability_secondary" : "", "ability_tertiary" : "",
+	"ice_primary" : "", "ice_secondary" : "", "ice_tertiary" : ""},
+
+	# Player 4 Colors
+	{"normal_primary" : "", "normal_secondary" : "", "normal_tertiary" : "",
+	"ability_primary" : "", "ability_secondary" : "", "ability_tertiary" : "",
+	"ice_primary" : "", "ice_secondary" : "", "ice_tertiary" : ""}
+]
---
+++ Scripts/MainMenu/PlayerColorsButton.gd
@@ -0,0 +1,15 @@
+extends "res://Scripts/MainMenu/BasicMenuButton.gd"
+
+export var ID = 0 # Player ID
+
+func _ready():
+	pass
+
+func _additional_processing():
+	Keymap.ActivePlayer = ID
+	
+#	var ControlIndicators = "SetControlsSet/ScrollContainer/HBoxContainer/ControlIndicators"
+#
+#	if Global.MainMenuNode.has_node(ControlIndicators):
+#		for i in Global.MainMenuNode.get_node(ControlIndicators).get_children():
+#			i.text = Keymap.PlayerControls[Keymap.ActivePlayer][i.name]
