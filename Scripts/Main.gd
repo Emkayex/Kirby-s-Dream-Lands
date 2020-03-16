2eb7d430edb79ad908d800f9ad89020626145915
Emkayex <53287857+Emkayex@users.noreply.github.com>
2020-03-15 22:30:27 -0500

    Forgot to Replace Video Settings Save/Load Functions

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -20,45 +20,22 @@ func _fps_timer_timeout():
 
 
 func load_video_settings():
-	var file = File.new()
-	var file_path = Global.OPTIONS_DIR + "//" + VIDEO_FILE
-
-	# Create variable to hold parsed JSON data
-	var dataJSON = JSONParseResult.new()
-
-	# Check if the controls file has already been generated from a previous game
-	if file.file_exists(file_path):
-
-		# Load raw text from controls file
-		file.open(file_path, File.READ)
-		var content = file.get_as_text()
-		file.close()
-
-		# Parse raw text as JSON
-		dataJSON = JSON.parse(content)
-		print(dataJSON.result)
-		
-		# Set video settings based upon parsed dict
-		$FPSLabel.visible = bool(dataJSON.result["display_fps"])
-		OS.window_fullscreen = bool(dataJSON.result["fullscreen"])
+	var data = Global.load_from_json(Global.OPTIONS_DIR, VIDEO_FILE)
 	
-	# Generate a default bus volume file if one hasn't been created
+	# Set video settings based upon parsed dict
+	if data.exists:
+		$FPSLabel.visible = bool(data.result["display_fps"])
+		OS.window_fullscreen = bool(data.result["fullscreen"])
+	
+	# Generate a default video settings file if one hasn't been created
 	else:
 		save_video_settings()
 
 
 func save_video_settings():
-	var file = File.new()
-	var file_path = Global.OPTIONS_DIR + "//" + VIDEO_FILE
-	
 	# Assemble a dictionary of the video settings
 	var video_dict = {}
 	video_dict["display_fps"] = $FPSLabel.visible
 	video_dict["fullscreen"] = OS.window_fullscreen
 	
-	# Bus volumes stored as JSON
-	var dataJSON = JSON.print(video_dict)
-	
-	file.open(file_path, File.WRITE)
-	file.store_string(dataJSON)
-	file.close()
+	Global.save_as_json(Global.OPTIONS_DIR, VIDEO_FILE, video_dict)
